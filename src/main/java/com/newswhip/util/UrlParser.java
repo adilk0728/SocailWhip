2291166dac7e142abc25e1614791d31e8b48a08b
adilk0728 <adithyaopeth@gmail.com>
2024-04-27 15:36:01 +0100

    Add URL parsing Logic

---
+++ src/main/java/com/newswhip/util/UrlParser.java
@@ -0,0 +1,29 @@
+package com.newswhip.util;
+
+public final class UrlParser {
+
+    private UrlParser(){}
+
+    public static String getSubdomain(String inputUrl){
+       int indexOfProtoEnd =  inputUrl.indexOf('/');
+       int indexOfSubDomainEnd = inputUrl.indexOf('.', indexOfProtoEnd) - 1;
+       return inputUrl.substring(indexOfProtoEnd + 2, indexOfSubDomainEnd + 1);
+    }
+
+    public static String getSecondLevelDomain(String inputUrl){
+       int indexOfSubDomainEnd = inputUrl.indexOf('.') - 1;
+       int indexOfSecondLvlEnd = inputUrl.indexOf('.', indexOfSubDomainEnd + 2) - 1;
+       return inputUrl.substring(indexOfSubDomainEnd + 2, indexOfSecondLvlEnd + 1);
+    }
+    public static String getTopLevelDomain(String inputUrl){
+        int indexOfSubDomainEnd = inputUrl.indexOf('.') - 1;
+        int indexOfSecondLvlEnd = inputUrl.indexOf('.', indexOfSubDomainEnd + 2) - 1;
+        int indexOfTldEnd = inputUrl.indexOf('/', indexOfSecondLvlEnd + 2) - 1;
+        return inputUrl.substring(indexOfSecondLvlEnd + 2, indexOfTldEnd + 1);
+    }
+
+//  TODO: Logic to extract path only if required
+//    public static String getPagePath(String inputUrl){
+//       return "";
+//    }
+}
